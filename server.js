5135cde5b34c33ea095458a0e9af4d99a54b869b
vishnu1997 <reddy.vishnu1997@gmail.com>
2016-09-18 10:50:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-two',function (req,res){
+    res.send("Article two will be sent ");
+});
+app.get('/article-three',function (req,res){
+    res.send("Article three will be sent ");
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
